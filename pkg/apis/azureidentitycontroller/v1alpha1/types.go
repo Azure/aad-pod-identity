d8a38b0a983bfec9b946e21e3fda18ac56cfe93a
Krishnakumar R <29471693+kkmsft@users.noreply.github.com>
2018-04-10 22:05:17 +0000

    Initial commit to checkpoint the work - CRD in yaml and the go structures.

---
+++ pkg/apis/azureidentitycontroller/v1alpha1/types.go
@@ -0,0 +1,60 @@
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// AzureIdentity is the specification of the identity datastructure.
+type AzureIdentity struct {
+	metav1.TypeMeta             `json:",inline"`
+	metav1.ObjectMeta           `json:"metadata,omitempty"`
+
+	Spec	AzureIdentitySpec   `json:"spec"`
+	Status  AzureIdentityStatus `json:"status"`
+}
+
+type IdentityType int
+
+const (
+	UserAssignedMSI IdentityType = 0
+	ServicePrincipal IdentityType = 1
+)
+
+type AzureIdentitySpec struct {
+	// EMSI or Service Principle
+	Type  IdentityType `json:"type"`
+	Id  string      `json:"id"`
+}
+
+type AzureIdentityStatus {
+	AvailableReplicas int32 `json:"availableRepliacs"`
+}
+
+
+type AzureIdentityBinding struct {
+	metav1.TypeMeta                    `json:",inline"`
+	metav1.ObjectMeta                  `json:"metadata,omitempty"`
+
+	Spec	AzureIdentityBindingSpec   `json:"spec"`
+	Status  AzureIdentityBindingStatus `json:"status"`
+}
+
+type MatchType int
+
+const (
+	Explicit  MatchType = 0
+	Selector  MatchType = 1
+)
+
+type AzureIdentityBindingSpec struct {
+	AzureIdRef *AzureIdentity
+	MatchType  MatchType
+	MatchName string
+	Weight int
+ }
+
+
+
+
+
