618925f1aa76d4700807277fe097d8b9b86194ca
Ernest Wong <chewong@uwaterloo.ca>
2018-11-12 15:38:30 -0800

    Modify identity validator to accompany cluster-wide and pod identity

--- test/e2e/config/config.go
+++ test/e2e/config/config.go
@@ -4,9 +4,12 @@ import "github.com/kelseyhightower/envconfig"
 
 // Config holds global test configuration translated from environment variables
 type Config struct {
-	SubscriptionID string `envconfig:"SUBSCRIPTION_ID"`
-	ResourceGroup  string `envconfig:"RESOURCE_GROUP"`
-	AzureClientID  string `envconfig:"AZURE_CLIENT_ID"`
+	SubscriptionID        string `envconfig:"SUBSCRIPTION_ID"`
+	ResourceGroup         string `envconfig:"RESOURCE_GROUP"`
+	AzureClientID         string `envconfig:"AZURE_CLIENT_ID"`
+	KeyvaultName          string `envconfig:"KEYVAULT_NAME"`
+	KeyvaultSecretName    string `envconfig:"KEYVAULT_SECRET_NAME"`
+	KeyvaultSecretVersion string `envconfig:"KEYVAULT_SECRET_VERSION"`
 }
 
 // ParseConfig will parse needed environment variables for running the tests
