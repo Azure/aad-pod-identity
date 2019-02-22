47a30e88ad4f1e65d198926bc913282bcb739a1b
vplauzon <vplauzon@vplauzon.com>
2019-02-22 16:40:39 -0500

    .

--- example/rest-api/service/script.sh
+++ example/rest-api/service/script.sh
@@ -7,7 +7,8 @@ while true
 do
     echo "Iteration $i"
 
-    curl  http://169.254.169.254/metadata/identity/oauth2/token/?resource=https://vault.azure.net
+    jwt=curl  http://169.254.169.254/metadata/identity/oauth2/token/?resource=https://vault.azure.net
+    echo $jwt
     echo '{"a" : 3, "b" : "john"}' | jq '.a'
     i=$((i+1))
     sleep 1
