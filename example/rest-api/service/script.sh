39c6621cee6ff0129a0a37d8818c38fd64712569
vplauzon <vplauzon@vplauzon.com>
2019-02-22 16:51:02 -0500

    .

--- example/rest-api/service/script.sh
+++ example/rest-api/service/script.sh
@@ -8,8 +8,8 @@ do
     echo "Iteration $i"
 
     jwt=$(curl http://169.254.169.254/metadata/identity/oauth2/token/?resource=https://vault.azure.net)
-    echo $jwt
-    echo '{"a" : 3, "b" : "john"}' | jq '.a'
+    token=echo $jwt | jq '.access_token'
+    echo "Token:  $token"
     i=$((i+1))
     sleep 1
 done
