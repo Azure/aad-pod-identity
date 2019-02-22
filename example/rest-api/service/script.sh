0a30c5d6f90806f20ecceaa7693e2cde038ca78e
vplauzon <vplauzon@vplauzon.com>
2019-02-22 16:54:40 -0500

    .

--- example/rest-api/service/script.sh
+++ example/rest-api/service/script.sh
@@ -8,7 +8,7 @@ do
     echo "Iteration $i"
 
     jwt=$(curl http://169.254.169.254/metadata/identity/oauth2/token/?resource=$RESOURCE)
-    token=echo $jwt | jq '.access_token'
+    token=$(echo $jwt | jq '.access_token')
     echo "Token:  $token"
     i=$((i+1))
     sleep 1
