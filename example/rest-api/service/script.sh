de3654ad2d5d1277d30a25ac26fededf31eba611
vplauzon <vplauzon@vplauzon.com>
2019-02-22 16:59:34 -0500

    .

--- example/rest-api/service/script.sh
+++ example/rest-api/service/script.sh
@@ -7,9 +7,11 @@ while true
 do
     echo "Iteration $i"
 
-    jwt=$(curl http://169.254.169.254/metadata/identity/oauth2/token/?resource=$RESOURCE)
+    jwt=$(curl -sS http://169.254.169.254/metadata/identity/oauth2/token/?resource=$RESOURCE)
+    echo "Full token:  $jwt"
     token=$(echo $jwt | jq '.access_token')
-    echo "Token:  $token"
+    echo "Access token:  $token"
+    
     i=$((i+1))
     sleep 1
 done
