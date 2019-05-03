e11627d6f8fc27a53b3793136dd10762aedae4b3
Anish Ramasekar <anishramasekar@Anishs-MBP.guest.corp.microsoft.com>
2019-05-03 11:18:19 -0700

    Add retry sleep interval as option

--- pkg/k8s/client_test.go
+++ pkg/k8s/client_test.go
@@ -2,6 +2,7 @@ package k8s
 
 import (
 	"testing"
+	"time"
 
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -28,5 +29,75 @@ func TestGetSecret(t *testing.T) {
 	}
 	if retrievedSecret.ObjectMeta.Name != secretName {
 		t.Fatalf("Incorrect secret name: %v", retrievedSecret.ObjectMeta.Name)
+	}
+}
+
+func TestGetPodName(t *testing.T) {
+	podIP := "10.0.0.8"
+
+	fakeClient := fake.NewSimpleClientset()
+
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "testpodname",
+			Namespace: "default",
+		},
+		Status: v1.PodStatus{
+			PodIP: podIP,
+		},
+	}
+	fakeClient.CoreV1().Pods("default").Create(pod)
+
+	kubeClient := &KubeClient{ClientSet: fakeClient}
+
+	podNs, podName, err := kubeClient.GetPodName(podIP)
+	if err != nil {
+		t.Fatalf("Error getting pod: %v", err)
+	}
+	if podName != "testpodname" {
+		t.Fatalf("Incorrect pod name: %v", podName)
+	}
+	if podNs != "default" {
+		t.Fatalf("Incorrect pod ns: %v", podNs)
+	}
+}
+
+func TestPodListRetries(t *testing.T) {
+	// this test is to solely test the retry and sleep logic works as expected
+	podIP := "10.0.0.8"
+
+	fakeClient := fake.NewSimpleClientset()
+
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "testpodname1",
+			Namespace: "default",
+		},
+		Status: v1.PodStatus{
+			PodIP: podIP,
+		},
+	}
+	kubeClient := &KubeClient{ClientSet: fakeClient}
+
+	time.AfterFunc(time.Duration(1200*time.Millisecond), func() {
+		fakeClient.CoreV1().Pods("default").Create(pod)
+	})
+
+	start := time.Now()
+	podNs, podName, err := kubeClient.GetPodName(podIP)
+	elapsed := time.Since(start)
+
+	if err != nil {
+		t.Fatalf("Error getting pod: %v", err)
+	}
+	if podName != "testpodname1" {
+		t.Fatalf("Incorrect pod name: %v", podName)
+	}
+	if podNs != "default" {
+		t.Fatalf("Incorrect pod ns: %v", podNs)
+	}
+	// check the retries actually work as the pod object is created only after 1.2s
+	if elapsed < 1200*time.Millisecond {
+		t.Fatalf("Retry logic not working as expected. Elapsed time: %v", elapsed)
 	}
 }
