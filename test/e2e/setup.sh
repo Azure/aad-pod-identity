bec0e2c8f886f3f6e5d0320115cdf880e793d828
Anish Ramasekar <anish.ramasekar@gmail.com>
2019-05-15 17:01:48 -0700

    update script

--- test/e2e/setup.sh
+++ test/e2e/setup.sh
@@ -33,7 +33,7 @@ echo 'Creating a cluster-identity and assign appropriate roles...'
 az identity create -g "$RESOURCE_GROUP" -n cluster-identity
 IDENTITY_CLIENT_ID=$(az identity show -g "$RESOURCE_GROUP" -n cluster-identity  --query 'clientId' -otsv)
 IDENTITY_PRINCIPAL_ID=$(az identity show -g "$RESOURCE_GROUP" -n cluster-identity --query 'principalId' -otsv)
-az role assignment create --role 'Managed Identity Operator' --assignee "$AZURE_CLIENT_ID" --scope "/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.ManagedIdentity/userAssignedIdentities/cluster-identity"
+az role assignment create --role 'Managed Identity Operator' --assignee "$AZURE_CLIENT_ID" --scope "/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.ManagedIdentity/userAssignedIdentities/cluster-identity" || true
 # The following command might need a couple of retries to succeed
 set +e
 for i in {0..10}; do
