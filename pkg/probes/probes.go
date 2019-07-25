4c0c7ec55a39af0e10bb0f9d4f90172670ff724e
Krishnakumar R(KK) <29471693+kkmsft@users.noreply.github.com>
2019-07-24 19:42:52 -0700

    Increase the first time check interval and return active/not active via readiness probe

--- pkg/probes/probes.go
+++ pkg/probes/probes.go
@@ -18,25 +18,39 @@ func InitHealthProbe() {
 // return an error (500). Also returned in the ready probe is the amount of time from the start of this container.
 func InitReadyProbe(startTime time.Time, condition *bool) {
 	http.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
+		// Readiness probe being 500 causes too many events to be produced.
+		// Hence we post the real status as result of the readiness probe instead of
+		// marking the state as not ready.
+		w.WriteHeader(200)
 		if *condition {
-			w.WriteHeader(200)
+			w.Write([]byte("Active"))
 		} else {
-			w.WriteHeader(500)
+			w.Write([]byte("Not Active"))
 		}
-		data := (time.Since(startTime)).String()
-		w.Write([]byte(data))
 	})
+}
+
+func startAsync(port string) {
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		glog.Fatalf("Http listen and serve error: %+v", err)
+	} else {
+		glog.V(1).Infof("Http listen and serve started !")
+	}
 }
 
 //Start - Starts the required http server to start the probe to respond.
 func Start(port string) {
-	glog.Fatal(http.ListenAndServe(":"+port, nil))
+	go startAsync(port)
 }
 
 // InitAndStart - Initialize the default probes and starts the http listening port.
 func InitAndStart(port string, startTime time.Time, condition *bool) {
 	InitHealthProbe()
+	glog.V(1).Infof("Initialized health probe")
 	InitReadyProbe(startTime, condition)
+	glog.V(1).Infof("Initialized readiness probe")
 	// start the probes.
 	Start(port)
+	glog.Infof("Initialized and started health and readiness probe")
 }
