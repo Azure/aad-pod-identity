b3aefdd61a34291719f5807ed3096b44e36c7491
Anish Ramasekar <anish.ramasekar@gmail.com>
2019-08-16 10:18:49 -0700

    Redact client id in NMI logs (#343)
    * Redact client id in logs
    
    * add unit tests for redact

--- pkg/nmi/server/utils.go
+++ pkg/nmi/server/utils.go
@@ -1,6 +1,10 @@
 package server
 
-import log "github.com/sirupsen/logrus"
+import (
+	"regexp"
+
+	log "github.com/sirupsen/logrus"
+)
 
 // Log - this struct abstracts the logging libraries into single interface.
 type Log struct {
@@ -14,4 +18,13 @@ func (l Log) Info(msg string) {
 // Errorf - log the messages to be error messages and formatted.
 func (l Log) Errorf(format string, args ...interface{}) {
 	log.Errorf(format, &args)
+}
+
+func redactClientID(clientID string) string {
+	return redact(clientID, "$1##### REDACTED #####$3")
+}
+
+func redact(src, repl string) string {
+	r, _ := regexp.Compile("^(\\S{4})(\\S|\\s)*(\\S{4})$")
+	return r.ReplaceAllString(src, repl)
 }
---
+++ pkg/nmi/server/utils_test.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"testing"
+)
+
+func TestRedactClientID(t *testing.T) {
+	tests := []struct {
+		name     string
+		clientID string
+		expected string
+	}{
+		{
+			name:     "should redact client id",
+			clientID: "aabc0000-a83v-9h4m-000j-2c0a66b0c1f9",
+			expected: "aabc##### REDACTED #####c1f9",
+		},
+	}
+
+	for _, test := range tests {
+		actual := redactClientID(test.clientID)
+		if actual != test.expected {
+			t.Fatalf("expected: %s, got %s", test.expected, actual)
+		}
+	}
+}
