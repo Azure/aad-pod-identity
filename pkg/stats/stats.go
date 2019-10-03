e47de720332c867b87fc3ae6ff90d2754ab49d4d
Anish Ramasekar <anish.ramasekar@gmail.com>
2019-10-02 18:05:15 -0700

    Add stats for PUT/GET and count (#407)
    * Add stats for PUT/GET and count
    
    * add print for count

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -8,37 +8,44 @@ import (
 )
 
 var GlobalStats map[StatsType]time.Duration
+var CountStats map[StatsType]int
+
 var Mutex *sync.RWMutex
 
 type StatsType string
 
 const (
-	Total                StatsType = "Total"
-	System               StatsType = "System"
-	CacheSync            StatsType = "CacheSync"
-	CurrentState         StatsType = "Gather current state"
-	PodList              StatsType = "Pod listing"
-	BindingList          StatsType = "Binding listing"
-	IDList               StatsType = "ID listing"
-	ExceptionList        StatsType = "Pod Identity Exception listing"
-	AssignedIDList       StatsType = "Assigned ID listing"
-	CloudGet             StatsType = "Cloud provider get"
-	CloudPut             StatsType = "Cloud provider put"
-	K8sGet               StatsType = "K8s get"
-	K8sPut               StatsType = "K8s put"
-	FindAssignedIDDel    StatsType = "Find assigned ids to delete"
-	FindAssignedIDCreate StatsType = "Find assigned ids to create"
-	AssignedIDDel        StatsType = "Assigned ID deletion"
-	AssignedIDAdd        StatsType = "Assigned ID addition"
-	TotalIDDel           StatsType = "Total time to delete assigned IDs"
-	TotalIDAdd           StatsType = "Total time to add assigned IDs"
-	TotalCreateOrUpdate  StatsType = "Total time to assign or remove IDs"
+	Total                   StatsType = "Total"
+	System                  StatsType = "System"
+	CacheSync               StatsType = "CacheSync"
+	CurrentState            StatsType = "Gather current state"
+	PodList                 StatsType = "Pod listing"
+	BindingList             StatsType = "Binding listing"
+	IDList                  StatsType = "ID listing"
+	ExceptionList           StatsType = "Pod Identity Exception listing"
+	AssignedIDList          StatsType = "Assigned ID listing"
+	CloudGet                StatsType = "Cloud provider get"
+	CloudPut                StatsType = "Cloud provider put"
+	TotalPutCalls           StatsType = "Number of cloud provider PUT"
+	TotalGetCalls           StatsType = "Number of cloud provider GET"
+	TotalAssignedIDsCreated StatsType = "Number of assigned ids created in this sync cycle"
+	TotalAssignedIDsDeleted StatsType = "Number of assigned ids deleted in this sync cycle"
+	K8sGet                  StatsType = "K8s get"
+	K8sPut                  StatsType = "K8s put"
+	FindAssignedIDDel       StatsType = "Find assigned ids to delete"
+	FindAssignedIDCreate    StatsType = "Find assigned ids to create"
+	AssignedIDDel           StatsType = "Assigned ID deletion"
+	AssignedIDAdd           StatsType = "Assigned ID addition"
+	TotalIDDel              StatsType = "Total time to delete assigned IDs"
+	TotalIDAdd              StatsType = "Total time to add assigned IDs"
+	TotalCreateOrUpdate     StatsType = "Total time to assign or remove IDs"
 
 	EventRecord StatsType = "Event recording"
 )
 
 func Init() {
 	GlobalStats = make(map[StatsType]time.Duration)
+	CountStats = make(map[StatsType]int)
 	Mutex = &sync.RWMutex{}
 }
 
@@ -70,7 +77,21 @@ func Update(key StatsType, val time.Duration) {
 func Print(key StatsType) {
 	Mutex.RLock()
 	defer Mutex.RUnlock()
+
 	glog.Infof("%s: %s", key, GlobalStats[key])
+}
+
+func PrintCount(key StatsType) {
+	Mutex.RLock()
+	defer Mutex.RUnlock()
+
+	glog.Infof("%s: %d", key, CountStats[key])
+}
+
+func UpdateCount(key StatsType, val int) {
+	Mutex.Lock()
+	defer Mutex.Unlock()
+	CountStats[key] = CountStats[key] + val
 }
 
 func PrintSync() {
@@ -88,6 +109,12 @@ func PrintSync() {
 		Print(CloudPut)
 		Print(AssignedIDAdd)
 		Print(AssignedIDDel)
+
+		PrintCount(TotalPutCalls)
+		PrintCount(TotalGetCalls)
+
+		PrintCount(TotalAssignedIDsCreated)
+		PrintCount(TotalAssignedIDsDeleted)
 
 		Print(FindAssignedIDCreate)
 		Print(FindAssignedIDDel)
