9af3b693a2e96b7570ef3a47923bd1babd3b3290
Krishnakumar R(KK) <29471693+kkmsft@users.noreply.github.com>
2019-10-03 17:06:21 -0700

    Remove unused GET in CreateOrUpdate

--- pkg/cloudprovider/vm.go
+++ pkg/cloudprovider/vm.go
@@ -57,12 +57,6 @@ func (c *VMClient) CreateOrUpdate(rg string, nodeName string, vm compute.Virtual
 		glog.Error(err)
 		return err
 	}
-
-	vm, err = future.Result(c.client)
-	if err != nil {
-		glog.Error(err)
-		return err
-	}
 	stats.UpdateCount(stats.TotalPutCalls, 1)
 	stats.Update(stats.CloudPut, time.Since(begin))
 	return nil
