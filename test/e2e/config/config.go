baafddf9cfbc565d843204f70c438a39a72dd831
Anish Ramasekar <anish.ramasekar@gmail.com>
2019-10-11 18:17:58 -0700

    update tests manifests to apps/v1

--- test/e2e/config/config.go
+++ test/e2e/config/config.go
@@ -10,10 +10,10 @@ type Config struct {
 	KeyvaultName             string `envconfig:"KEYVAULT_NAME"`
 	KeyvaultSecretName       string `envconfig:"KEYVAULT_SECRET_NAME"`
 	KeyvaultSecretVersion    string `envconfig:"KEYVAULT_SECRET_VERSION"`
-	MICVersion               string `envconfig:"MIC_VERSION" default:"1.5.2"`
-	NMIVersion               string `envconfig:"NMI_VERSION" default:"1.5.2"`
+	MICVersion               string `envconfig:"MIC_VERSION" default:"1.5.3"`
+	NMIVersion               string `envconfig:"NMI_VERSION" default:"1.5.3"`
 	Registry                 string `envconfig:"REGISTRY" default:"mcr.microsoft.com/k8s/aad-pod-identity"`
-	IdentityValidatorVersion string `envconfig:"IDENTITY_VALIDATOR_VERSION" default:"1.5.2"`
+	IdentityValidatorVersion string `envconfig:"IDENTITY_VALIDATOR_VERSION" default:"1.5.3"`
 	SystemMSICluster         bool   `envconfig:"SYSTEM_MSI_CLUSTER" default:"false"`
 	EnableScaleFeatures      bool   `envconfig:"ENABLE_SCALE_FEATURES" default:"false"`
 }
