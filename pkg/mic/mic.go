a875e7cf525a68f0ec74f15748f2572ce63c217a
Anish Ramasekar <anish.ramasekar@gmail.com>
2019-11-21 11:36:27 -0800

    fix identity removal

--- pkg/mic/mic.go
+++ pkg/mic/mic.go
@@ -120,7 +120,8 @@ func NewMICClient(cloudconfig string, config *rest.Config, isNamespaced bool, sy
 
 	var immutableUserMSIsMap map[string]bool
 
-	if immutableUserMSIsList != nil {
+	if len(immutableUserMSIsList) > 0 {
+		// this map contains list of identities that are configured by user as immutable.
 		immutableUserMSIsMap = make(map[string]bool)
 		for _, item := range immutableUserMSIsList {
 			immutableUserMSIsMap[strings.ToLower(item)] = true
@@ -470,12 +471,16 @@ func (c *Client) getListOfIdsToDelete(deleteList map[string]aadpodid.AzureAssign
 			glog.Error(err)
 			continue
 		}
+
 		id := delID.Spec.AzureIdentityRef
 		isUserAssignedMSI := c.checkIfUserAssignedMSI(id)
+		isImmutableIdentity := c.checkIfIdentityImmutable(id.Spec.ResourceID)
 
 		// this case includes Assigned state and empty state to ensure backward compatability
 		if delID.Status.Status == aadpodid.AssignedIDAssigned || delID.Status.Status == "" {
-			if !inUse && isUserAssignedMSI {
+			// only user assigned identities that are not in use and are not defined as
+			// immutable will be removed from underlying node/vmss
+			if !inUse && isUserAssignedMSI && !isImmutableIdentity {
 				c.appendToRemoveListForNode(id.Spec.ResourceID, delID.Spec.NodeName, nodeMap)
 			}
 		}
@@ -805,20 +810,7 @@ func (c *Client) updateUserMSI(newAssignedIDs map[string]aadpodid.AzureAssignedI
 	addUserAssignedMSIIDs := c.getUniqueIDs(nodeTrackList.addUserAssignedMSIIDs)
 	removeUserAssignedMSIIDs := c.getUniqueIDs(nodeTrackList.removeUserAssignedMSIIDs)
 
-	// The the list of identities (user-defined managed identities) shouldn't be deleted from VM/VMSS, but only from Kubernetes
-	var approvedRemoveUserAssignedMSIIDs []string
-	if c.ImmutableUserMSIs != nil {
-		approvedRemoveUserAssignedMSIIDs := make([]string, 0)
-		for _, item := range removeUserAssignedMSIIDs {
-			if _, ok := c.ImmutableUserMSIs[item]; !ok {
-				approvedRemoveUserAssignedMSIIDs = append(approvedRemoveUserAssignedMSIIDs, item)
-			}
-		}
-	} else {
-		approvedRemoveUserAssignedMSIIDs = removeUserAssignedMSIIDs
-	}
-
-	err := c.CloudClient.UpdateUserMSI(addUserAssignedMSIIDs, approvedRemoveUserAssignedMSIIDs, nodeOrVMSSName, nodeTrackList.isvmss)
+	err := c.CloudClient.UpdateUserMSI(addUserAssignedMSIIDs, removeUserAssignedMSIIDs, nodeOrVMSSName, nodeTrackList.isvmss)
 	if err != nil {
 		glog.Errorf("Updating msis on node %s, add [%d], del [%d] failed with error %v", nodeOrVMSSName, len(nodeTrackList.assignedIDsToCreate), len(nodeTrackList.assignedIDsToDelete), err)
 		idList, getErr := c.getUserMSIListForNode(nodeOrVMSSName, nodeTrackList.isvmss)
@@ -1043,4 +1035,19 @@ func (c *Client) consolidateVMSSNodes(nodeMap map[string]trackUserAssignedMSIIds
 			nodeMap[getVMSSName(vmssName)] = vmssTrackList
 		}
 	}
+}
+
+// checkIfIdentityImmutable checks if the identity is immutable
+// if identity is immutable, then it will not be removed from underlying node/vmss
+// returns true if identity is immutable
+func (c *Client) checkIfIdentityImmutable(id string) bool {
+	// no immutable identity list defined, then identity is not immutable and can be safely removed
+	if c.ImmutableUserMSIs == nil {
+		return false
+	}
+	// identity is immutable, so should not be deleted from the underlying node/vmss
+	if _, exists := c.ImmutableUserMSIs[id]; exists {
+		return true
+	}
+	return false
 }
