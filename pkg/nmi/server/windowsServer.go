fd16843e8b1507fb2a2b885a03f50a7f5e5a0a37
bo wu <bowu@microsoft.com>
2020-02-07 03:06:26 +0800

    Add route policy cleanup

--- pkg/nmi/server/windowsServer.go
+++ pkg/nmi/server/windowsServer.go
@@ -4,6 +4,9 @@ package server
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog"
@@ -34,14 +37,23 @@ func (s *WindowsServer) Run() error {
 
 func (s *WindowsServer) Sync() {
 	klog.Info("Sync thread started.")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT)
+	s.Server.Initialized = true
+
 	var pod *v1.Pod
 
 	for {
 		select {
+		case <-signalChan:
+			s.DeleteRoutePolicyForExistingPods()
+			break
 		case pod = <-s.Server.PodObjChannel:
 			klog.V(6).Infof("Received event: %s", pod)
 
-			fmt.Printf("Host IP, Pod Node Name and Pod IP:%s %s %s \n", pod.Status.HostIP, pod.Spec.NodeName, pod.Status.PodIP)
+			fmt.Printf("Windows Server Host IP, Pod Node Name and Pod IP:%s %s %s \n", pod.Status.HostIP, pod.Spec.NodeName, pod.Status.PodIP)
+			fmt.Printf("Windows Server Pod Name:%s \n", pod.Name)
 			if s.Server.NodeName == pod.Spec.NodeName {
 				applyRoutePolicy(pod.Status.PodIP)
 			}
@@ -50,7 +62,7 @@ func (s *WindowsServer) Sync() {
 }
 
 func (s *WindowsServer) ApplyRoutePolicyForExistingPods() {
-	klog.Info("Apply existing pods started.")
+	klog.Info("Apply route pllicy for existing pods started.")
 
 	listPods, err := s.Server.PodClient.ListPods()
 	if err != nil {
@@ -65,7 +77,38 @@ func (s *WindowsServer) ApplyRoutePolicyForExistingPods() {
 	}
 }
 
+func (s *WindowsServer) DeleteRoutePolicyForExistingPods() {
+	klog.Info("Received SIGTERM, shutting down")
+	klog.Info("Delete route policy for existing pods started.")
+
+	listPods, err := s.Server.PodClient.ListPods()
+	if err != nil {
+		klog.Error(err)
+	}
+
+	for _, podItem := range listPods {
+		fmt.Printf("Host IP, Node Name and Pod IP: \n %s %s %s \n", podItem.Status.HostIP, podItem.Spec.NodeName, podItem.Status.PodIP)
+		if podItem.Spec.NodeName == s.Server.NodeName {
+			deleteRoutePolicy(podItem.Status.PodIP)
+		}
+	}
+}
+
 func applyRoutePolicy(podIP string) {
+
+	// Retrieve all the endpoints
+	// var endpoints = HCNProxy.EnumerateEndpoints()
+
+	// Foreach ennpoint, find the one for the podinfo and apply route policy
+	//for _, val := range endpoints {
+	//	if podIP == val.podIP {
+	//		HCNProxy.ApplyRoutePolicy(val)
+	//		break
+	//	}
+	//}
+}
+
+func deleteRoutePolicy(podIP string) {
 
 	// Retrieve all the endpoints
 	// var endpoints = HCNProxy.EnumerateEndpoints()
