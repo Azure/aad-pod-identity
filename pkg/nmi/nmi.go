8d8bb13af4b2d8344d49bd06132c57aec55e870b
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2020-03-25 10:50:21 -0500

    Handle MSI auth requests by ResourceID
    Filter identities by either ClientID or by ResourceID,
    depending on which was specified when authenticating.
    Neither is required but they are mutually exclusive.

--- pkg/nmi/nmi.go
+++ pkg/nmi/nmi.go
@@ -42,7 +42,7 @@ const (
 type TokenClient interface {
 	// GetIdentities gets the list of identities which match the
 	// given pod in the form of AzureIdentity.
-	GetIdentities(ctx context.Context, podns, podname, clientID string) (*aadpodid.AzureIdentity, error)
+	GetIdentities(ctx context.Context, podns, podname, clientID, resourceID string) (*aadpodid.AzureIdentity, error)
 	// GetToken acquires a token by using the AzureIdentity.
 	GetToken(ctx context.Context, clientID, resource string, podID aadpodid.AzureIdentity) (token *adal.Token, err error)
 }
--- pkg/nmi/server/server_test.go
+++ pkg/nmi/server/server_test.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 )
@@ -82,5 +84,66 @@ func TestMsiHandler_NoRemoteAddress(t *testing.T) {
 	expected := "request remote address is empty"
 	if expected != strings.TrimSpace(recorder.Body.String()) {
 		t.Errorf("Unexpected response body %s", recorder.Body.String())
+	}
+}
+
+func TestParseTokenRequest(t *testing.T) {
+	const endpoint = "http://127.0.0.1/metadata/identity/oauth2/token"
+
+	t.Run("query present", func(t *testing.T) {
+		const resource = "https://vault.azure.net"
+		const clientID = "77788899-f67e-42e1-9a78-89985f6bff3e"
+		const resourceID = "/subscriptions/9f2be85c-f8ae-4569-9353-38e5e8b459ef/resourcegroups/test/providers/Microsoft.ManagedIdentity/userAssignedIdentities/test"
+
+		var r http.Request
+		r.URL, _ = url.Parse(fmt.Sprintf("%s?client_id=%s&msi_res_id=%s&resource=%s", endpoint, clientID, resourceID, resource))
+
+		result := parseTokenRequest(&r)
+
+		if result.ClientID != clientID {
+			t.Errorf("invalid ClientID - expected: %q, actual: %q", clientID, result.ClientID)
+		}
+
+		if result.ResourceID != resourceID {
+			t.Errorf("invalid ResourceID - expected: %q, actual: %q", resourceID, result.ResourceID)
+		}
+
+		if result.Resource != resource {
+			t.Errorf("invalid Resource - expected: %q, actual: %q", resource, result.Resource)
+		}
+	})
+
+	t.Run("bare endpoint", func(t *testing.T) {
+		var r http.Request
+		r.URL, _ = url.Parse(endpoint)
+
+		result := parseTokenRequest(&r)
+
+		if result.ClientID != "" {
+			t.Errorf("invalid ClientID - expected: %q, actual: %q", "", result.ClientID)
+		}
+
+		if result.ResourceID != "" {
+			t.Errorf("invalid ResourceID - expected: %q, actual: %q", "", result.ResourceID)
+		}
+
+		if result.Resource != "" {
+			t.Errorf("invalid Resource - expected: %q, actual: %q", "", result.Resource)
+		}
+	})
+}
+
+func TestTokenRequest_ValidateResourceParamExists(t *testing.T) {
+	tr := TokenRequest{
+		Resource: "https://vault.azure.net",
+	}
+
+	if !tr.ValidateResourceParamExists() {
+		t.Error("ValidateResourceParamExists should have returned true when the resource is set")
+	}
+
+	tr.Resource = ""
+	if tr.ValidateResourceParamExists() {
+		t.Error("ValidateResourceParamExists should have returned false when the resource is unset")
 	}
 }
