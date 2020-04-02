9f8e6950f321663d8e98e047ae38871aaafa6c65
Anish Ramasekar <anish.ramasekar@gmail.com>
2020-04-02 12:16:42 -0700

    add validation for resource id (#548)
    * add validation for resource id
    
    * add conditional check for type 0

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "regexp"
+import (
+	"fmt"
+	"regexp"
+)
 
 // RedactClientID redacts client id
 func RedactClientID(clientID string) string {
@@ -10,4 +13,14 @@ func RedactClientID(clientID string) string {
 func redact(src, repl string) string {
 	r, _ := regexp.Compile("^(\\S{4})(\\S|\\s)*(\\S{4})$")
 	return r.ReplaceAllString(src, repl)
+}
+
+// ValidateResourceID validates the resourceID is is of the format
+// `/subscriptions/<subid>/resourcegroups/<resourcegroup>/providers/Microsoft.ManagedIdentity/userAssignedIdentities/<name>`
+func ValidateResourceID(resourceID string) error {
+	isValid := regexp.MustCompile(`(?i)/subscriptions/(.+?)/resourcegroups/(.+?)/providers/Microsoft.ManagedIdentity/userAssignedIdentities/(.+)`).MatchString
+	if !isValid(resourceID) {
+		return fmt.Errorf("Invalid resource id: %q, must match /subscriptions/<subid>/resourcegroups/<resourcegroup>/providers/Microsoft.ManagedIdentity/userAssignedIdentities/<name>", resourceID)
+	}
+	return nil
 }
