6254c3122f392092878079cefc029692074d857a
Ernest Wong <chuwon@microsoft.com>
2020-06-17 17:17:23 -0700

    test: convert e2e test cases from old to new framework (part 1)

--- test/e2e_new/framework/exec/kubectl.go
+++ test/e2e_new/framework/exec/kubectl.go
@@ -8,16 +8,32 @@ import (
 	"strings"
 
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 )
 
 // KubectlApply executes "kubectl apply" given a list of arguments.
-func KubectlApply() {
-	// TODO
+func KubectlApply(kubeconfigPath, namespace string, args []string) error {
+	args = append([]string{
+		"apply",
+		fmt.Sprintf("--kubeconfig=%s", kubeconfigPath),
+		fmt.Sprintf("--namespace=%s", namespace),
+	}, args...)
+
+	return kubectl(args)
+}
+
+// KubectlDelete executes "kubectl delete" given a list of arguments.
+func KubectlDelete(kubeconfigPath, namespace string, args []string) error {
+	args = append([]string{
+		"delete",
+		fmt.Sprintf("--kubeconfig=%s", kubeconfigPath),
+		fmt.Sprintf("--namespace=%s", namespace),
+	}, args...)
+
+	return kubectl(args)
 }
 
 // KubectlExec executes "kubectl exec" given a list of arguments.
-func KubectlExec(kubeconfigPath, podName, namespace string, args []string) {
+func KubectlExec(kubeconfigPath, podName, namespace string, args []string) error {
 	args = append([]string{
 		"exec",
 		fmt.Sprintf("--kubeconfig=%s", kubeconfigPath),
@@ -26,11 +42,15 @@ func KubectlExec(kubeconfigPath, podName, namespace string, args []string) {
 		"--",
 	}, args...)
 
+	return kubectl(args)
+}
+
+func kubectl(args []string) error {
 	By(fmt.Sprintf("kubectl %s", strings.Join(args, " ")))
 
 	cmd := exec.Command("kubectl", args...)
 	stdoutStderr, err := cmd.CombinedOutput()
 	fmt.Printf("%s", stdoutStderr)
 
-	Expect(err).To(BeNil())
+	return err
 }
