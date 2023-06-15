974b456a3cdee966c859b90a876da270b068a9ca
Anish Ramasekar <anish.ramasekar@gmail.com>
2023-06-15 23:23:36 +0000

    release: update manifest and helm charts for v1.8.17
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- test/e2e/framework/config.go
+++ test/e2e/framework/config.go
@@ -22,10 +22,10 @@ type Config struct {
 	KeyvaultName                       string        `envconfig:"KEYVAULT_NAME"`
 	KeyvaultSecretName                 string        `envconfig:"KEYVAULT_SECRET_NAME"`
 	KeyvaultSecretVersion              string        `envconfig:"KEYVAULT_SECRET_VERSION"`
-	MICVersion                         string        `envconfig:"MIC_VERSION" default:"v1.8.16"`
-	NMIVersion                         string        `envconfig:"NMI_VERSION" default:"v1.8.16"`
+	MICVersion                         string        `envconfig:"MIC_VERSION" default:"v1.8.17"`
+	NMIVersion                         string        `envconfig:"NMI_VERSION" default:"v1.8.17"`
 	Registry                           string        `envconfig:"REGISTRY" default:"mcr.microsoft.com/oss/azure/aad-pod-identity"`
-	IdentityValidatorVersion           string        `envconfig:"IDENTITY_VALIDATOR_VERSION" default:"v1.8.16"`
+	IdentityValidatorVersion           string        `envconfig:"IDENTITY_VALIDATOR_VERSION" default:"v1.8.17"`
 	EnableScaleFeatures                bool          `envconfig:"ENABLE_SCALE_FEATURES" default:"true"`
 	ImmutableUserMSIs                  string        `envconfig:"IMMUTABLE_IDENTITY_CLIENT_ID"`
 	NMIMode                            string        `envconfig:"NMI_MODE" default:"standard"`
